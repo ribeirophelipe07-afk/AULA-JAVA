cbc67f6075480e7349e20bb38e4322330f559230
ribeirophelipe07-afk <ribeirophelipe07@gmail.com>
2025-10-06 21:16:01 -0300

    Add files via upload

---
+++ ph22_09/src/Main.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+
+
+
+            JOptionPane.showMessageDialog(null, "seja bem vindo");
+
+            String raioStr = JOptionPane.showInputDialog("Digite o raio do círculo (número inteiro): " ) ;
+
+
+            double volume = Math.PI * Math.pow( raio, 3);
+
+                 // Saída de dados formatada
+            String mensagem = String.format("O volume do círculo com raio %d é:  %.2f", raio, volume);
+
+
+
+
+
+         }
+
+    }
